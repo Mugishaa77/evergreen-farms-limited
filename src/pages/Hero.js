36140e23e945bdd23082543909591338ebd58e4c
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-14 22:44:26 +0300

    Changed the font and color

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Hero.css';
-import open from "../logo/open.png";
+import hanging from "../logo/hanging.jpg";
 
 export default function Hero() {
   return (
     <div className="hero">
       <div className="position-relative">
         <div className="position-absolute top-0 end-0">
-          <img src={open} width="160" height="160" alt="open 24hrs" />
+          <img src={hanging} width="320" height="160" alt="open 24hrs" />
         </div>
         <div className="text-below-image">
             <h2>Your one stop shop for all your<br/>
