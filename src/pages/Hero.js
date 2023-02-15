43c31bc6297565dc825c13765c05ef8b97b428f3
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-15 10:22:52 +0300

    deleted duplicate files and fixed file paths

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Hero.css';
-import hanging from "../logo/hanging.jpg";
+import open from "../logo/open.png";
 
 export default function Hero() {
   return (
     <div className="hero">
       <div className="position-relative">
         <div className="position-absolute top-0 end-0">
-          <img src={hanging} width="320" height="160" alt="open 24hrs" />
+          <img src={open} width="160" height="160" alt="open 24hrs" />
         </div>
         <div className="text-below-image">
             <h2>Your one stop shop for all your<br/>
