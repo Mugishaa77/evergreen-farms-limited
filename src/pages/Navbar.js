8485f71def5a4d568885fa1b5a2dfdf5ddff3d37
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-15 11:27:25 +0300

    enhanced the navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import evergreen from '../logo/evergreen.jpg';
+import evergreen from '../logo/sharpened-evergreen.png';
 import { BasketFill } from "react-bootstrap-icons";
 import './Navbar.css';
 
@@ -10,32 +10,32 @@ export default function Navbar () {
     <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-style">
   <div className="container-fluid">
     <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="/">
 <img src= {evergreen} width="150" height="130" alt=""></img>
     </a>
   </div>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+          <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="#">Market</a>
+          <a className="nav-link active text-light" aria-current="page" href="/">Market</a>
         </li> 
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle text-light" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <BasketFill color="white" size={24} />
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item text-primary" href="#">Add to basket</a></li>
-            <li><a className="dropdown-item text-primary" href="#">Empty basket</a></li>
+            <li><a className="dropdown-item text-primary" href="/">Add to basket</a></li>
+            <li><a className="dropdown-item text-primary" href="/">Empty basket</a></li>
             <li><hr className="dropdown-divider text-primary"/></li>
-            <li><a className="dropdown-item text-primary" href="#">Something else here</a></li>
+            <li><a className="dropdown-item text-primary" href="/">Something else here</a></li>
           </ul>
         </li>
       </ul>
       <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="#">Contact Us</a>
+          <a className="nav-link active text-light" aria-current="page" href="/">Contact Us</a>
         </li>
     </div>
   </div>
