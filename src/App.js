0ce21dfb98cbf6dd8ad74c2c271abbce0c8a207f
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-15 21:38:17 +0300

    trial and error

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 import Navbar from './pages/Navbar';
 import Hero from './pages/Hero';
 import Footer from './pages/Footer';
-import Products from './pages/Products';
 import Category from './pages/Category';
-
-
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -14,8 +12,8 @@ function App() {
       <Navbar />
       <Hero />
       <div className="row">
-        <Category />
-      <Products />
+       <Category />
+       <Products />
        </div>
        <Footer />
      
