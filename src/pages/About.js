53568be3d21c20396991359548072897e29007b6
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-16 10:16:52 +0300

    Added context to the about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,46 @@ export default function About () {
         <div className="about">
             <div className="container">
                 <h1>About Us</h1>
-                <h6>Check again in afew days,,repeat after afew more🤭 </h6>
+
+<h6>Our Story</h6>
+<p>
+    Evergreen Farms was started as a small scale vegetable home orchard in Nyalenda slums in
+                   Kisumu, Kenya by Leisley Odhiambo (Odhis). 
+                   The name Evergreen derives from the fact that amid the difficulties and challenges, the
+residents of Nyalenda had an oasis of fresh farm produce, within a desert of strife and
+pandemonium  the ever green farm. farm. Later, it became Evergreen Farms when Odhis decided
+to take it up as a full-time business. 
+</p>
+
+<h6>Our Team</h6>
+<p> Odhis started a small mentorship group whose agenda was to inspire the young men and women of Nyalenda
+    where he met Lavender, Judy and Eric, who all joined
+    hands to become, <strong>the Green Team (Founding Four)</strong> with whom
+they started what has come to be known as Evergreen Farms.
+</p>
+
+<h6>We Believe</h6>
+<p>“...We make
+a living out
+of what we
+get, but we
+make life,
+out of what
+we give...”</p>
+
+<h6>How we operate</h6>
+<p>Evergreen's Mobile Garden innovation creates a direct nexus between farmers,
+grocers and the end-consumers hence increasing the farmer's and grocer's profits while
+reducing the cost of food by almost half.</p>
+
+<h6>Our Vision</h6>
+<p>Farmers will sell their produce to grocers at their desired prices,
+     The grocers also will in turn sell to the ultimate consumers at fairer prices, making
+<strong>food affordable</strong> altogether. 
+It also <strong>increases profits</strong> to both the farmer and the grocer,
+resulting in <strong>better living standards, and investment in farming as an economic activity within
+Africa.</strong> 
+</p>
             </div>
         </div>
     );
