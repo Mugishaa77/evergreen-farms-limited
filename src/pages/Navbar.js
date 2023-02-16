a5a32d944b0b1974c50e96d22574aef081758b30
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-16 14:07:22 +0300

    still working on the mobile responsiveness

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -7,39 +7,41 @@ import './Navbar.css';
 export default function Navbar () {
     return (
     <div>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-style">
-  <div className="container-fluid">
-    <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-style">
+  <div class="container-fluid">
     <a class="navbar-brand" href="/">
-<img src= {evergreen} width="220" height="130" alt=""></img>
+      <img src={evergreen} width="220" height="130" alt=""/>
     </a>
-  </div>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="/">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="/">Market</a>
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="/">Market</a>
         </li> 
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle text-light" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <BasketFill color="white" size={24} />
           </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item text-primary" href="/">Add to basket</a></li>
-            <li><a className="dropdown-item text-primary" href="/">Empty basket</a></li>
-            <li><hr className="dropdown-divider text-primary"/></li>
-            <li><a className="dropdown-item text-primary" href="/">Something else here</a></li>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item text-primary" href="/">Add to basket</a></li>
+            <li><a class="dropdown-item text-primary" href="/">Empty basket</a></li>
+            <li><hr class="dropdown-divider text-primary"/></li>
+            <li><a class="dropdown-item text-primary" href="/">Something else here</a></li>
           </ul>
         </li>
-      </ul>
-      <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="/">Contact Us</a>
+        <li class="nav-item">
+          <a class="nav-link active text-light" href="/">Contact Us</a>
         </li>
+      </ul>
     </div>
   </div>
 </nav>
+
     </div>
     );
 } 
