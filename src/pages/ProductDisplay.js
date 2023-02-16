a9daeb1ab4ea155b71383da65fa7303f2e345baf
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-16 22:34:52 +0300

    testing

--- src/pages/ProductDisplay.js
+++ src/pages/ProductDisplay.js
@@ -68,7 +68,7 @@ export default function ProductDisplay() {
   ));
 
   return (
-    <Slider {...settings}>
+    <Slider {...settings} className="my-carousel"> 
       {productCards}
     </Slider>
   );
