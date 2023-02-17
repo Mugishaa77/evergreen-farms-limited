5b81397d7fa6222bc6b562ab3ee2b98cbe22bae9
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-17 11:46:14 +0300

    settled on a display format

--- src/pages/ProductDisplay.js
+++ src/pages/ProductDisplay.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import Slider from 'react-slick';
-import './Products.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-import tomatoes from '../produce/tomatoes.jpg';
-import garlic from '../produce/garlic.jpg';
-import redOnions from '../produce/red-onions.jpg';
 import sagaa from '../produce/sagaa.jpg';
 import springOnions from '../produce/spring-onions.jpg';
-
-const ArrowLeft = props => (
-  <button className="slick-arrow slick-prev">Previous</button>
-);
-
-const ArrowRight = props => (
-  <button className="slick-arrow slick-next">Next</button>
-);
+import tomatoes from '../produce/tomatoes.jpg';
+import redOnions from '../produce/red-onions.jpg';
+import garlic from '../produce/garlic.jpg';
 
 const products = [
-  {name: 'Tomatoes', image: tomatoes, price: 'Ksh 10.00'},
-  {name: 'Garlic', image: garlic, price: 'Ksh 10.00'},
+  {name: 'Tomatoes',
+   image: tomatoes,
+  price: 'Ksh 10.00'},
+  {
+    name: 'Garlic',
+    image: garlic,
+    price: 'Ksh 10.00'
+  },
   {
     name: 'Red Onions',
     image: redOnions,
@@ -36,30 +32,21 @@ const products = [
     image: springOnions,
     price: 'Ksh 10.00'
   }
+  
 ];
-
-const ProductDisplay = () => {
+export default function ProductDisplay() {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 1,
-    prevArrow: <ArrowLeft />,
-    nextArrow: <ArrowRight />,
-    appendDots: dots => (
-      <div style={{ textAlign: "center" }}>
-        <span>
-          Slide {dots.props.currentSlide + 1} of {dots.props.slideCount}
-        </span>
-      </div>
-    ),
+    slidesToScroll: 2,
     responsive: [
       {
-        breakpoint: 900,
+        breakpoint: 768,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1
+          slidesToScroll: 2
         }
       },
       {
@@ -72,26 +59,20 @@ const ProductDisplay = () => {
     ]
   };
 
-  return (
-    <div className="product-carousel">
-      <div className="container">
-      <Slider {...settings}>
-        {products.map((product, index) => (
-          <div key={index}>
-            <div className="display">
-              <h1>Site Testing....</h1>
-              <img className="img-thumbnail rounded"
-              src={product.image} alt={product.name} />
-              <h2>{product.name}</h2>
-              <p>{product.price}</p>
-              <button>Buy Now</button>
-            </div>
-          </div>
-        ))}
-      </Slider>
+  const productCards = products.map((product, index) => (
+    <div className="display" key={index}>
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>{product.price}</p>
+      <button onClick={() => console.log(`Buy ${product.name} clicked!`)}>
+        Buy Now
+      </button>
     </div>
-    </div>
-  );
-};
+  ));
 
-export default ProductDisplay;
+  return (
+    <Slider {...settings}>
+      {productCards}
+    </Slider>
+  );
+}
