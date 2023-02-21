be40d8e60a025d9df294bed95b894843edf3c6a7
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-21 14:38:18 +0300

    Cleaned  up the display component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import { useState } from 'react';
+import React from 'react';
 import Navbar from './pages/Navbar';
 import Hero from './pages/Hero';
 import Category from './pages/Category';
 import ProductDisplay from './pages/ProductDisplay';
 import About from './pages/About';
 import Footer from './pages/Footer';
-import Basket from './pages/Basket';
-import products from './ProductData';
 
-function App() {
-  const [basketItems, setBasketItems] = useState([]);
-
-  const handleAddToBasket = (product) => {
-    setBasketItems([...basketItems, product]);
-  };
-
-  const handleRemoveFromBasket = (product) => {
-    setBasketItems(basketItems.filter((item) => item !== product));
-  };
+function App () {
 
   return (
     <div className="App">
@@ -25,17 +14,7 @@ function App() {
       <Hero />
       <div className="row">
         <Category />
-        <ProductDisplay
-          products={products}
-          basketItems={basketItems}
-          handleAddToBasket={handleAddToBasket}
-          handleRemoveFromBasket={handleRemoveFromBasket}
-        />
-        <Basket
-          basketItems={basketItems}
-          handleAddToBasket={handleAddToBasket}
-          handleRemoveFromBasket={handleRemoveFromBasket}
-        />
+        <ProductDisplay />
         <About />
       </div>
       <Footer />
