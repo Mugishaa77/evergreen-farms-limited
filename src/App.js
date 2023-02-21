0d8cd0a5f498cd82d3474d863f295c5ae38978ed
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-21 16:14:37 +0300

    added a 404 not found web page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './pages/Navbar';
-import Hero from './pages/Hero';
 import Basket from './pages/Basket';
-import Category from './pages/Category';
-import ProductDisplay from './pages/ProductDisplay';
-import About from './pages/About';
-import Footer from './pages/Footer';
+import NotFound from './pages/NotFound';
+
 
 function App () {
-
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar />
+
       <Routes>
-        <Route path="/basket" component={Basket} />
-        <Route path="/hero" component={Hero} />
+          
+          <Route path="/" exact element={<Basket/>} />
+          
+          
+          
+          
+          
       </Routes>
+      
       </BrowserRouter>
     </div>
   );
