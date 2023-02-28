34e31e4848359ae71ff38cc58bd4e094de5c525b
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-28 19:26:22 +0300

    implemented redux function

---
+++ src/features/basketSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    basketItems: [],
+    basketTotalQuantity: 0,
+    basketTotalAmount: 0,
+};
+
+const basketSlice = createSlice({
+    name: "basket",
+    initialState,
+    reducers: {
+        addToBasket(state, action){
+            const itemIndex = state.basketItems.findIndex((item) => item.id === action.payload.id);
+            if(itemIndex >= 0){
+                state.basketItems[itemIndex].basketQuantity += 1
+            } else {
+                const tempProduct = {...action.payload, basketQuantity: 1 };
+                state.basketItems.push(tempProduct);
+            }
+        },
+    },
+});
+
+export const { addToBasket } = basketSlice.actions;
+
+export default basketSlice.reducer;
+
