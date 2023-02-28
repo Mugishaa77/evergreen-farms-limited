a553ec675dd636a9a34914ffee4150258f8b2718
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-02-28 15:13:10 +0300

    Added products for display to the UI

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,11 +1,29 @@
 import React from 'react';
+
 import { useGetAllProductsQuery } from '../features/productsApi';
 
 export default function Basket () {
-    const { data,error, isLoading } = useGetAllProductsQuery()
+    
+    const { data, error, isLoading } = useGetAllProductsQuery();
     return (
         <div className="basket">
-            Basket Component
+            { isLoading ? (<p>Loading....</p>) :
+             error ? (<p>an error occured</p>) :
+             (<>
+             <h2>New Arrivals</h2>
+             <div className="products">
+                {data?.map(product => <div key={product.id} className="product">
+                    <h3>{product.name}</h3>
+                    <img src={product.image} alt={product.name} />
+                    <div className="details">
+                        <span>{product.desc}</span>
+                        <span className="price">Kshs {product.price}</span>
+                    </div>
+                    <button>Add to Basket</button>
+
+                </div>)}
+                </div></>)
+            }
         </div>
     );
 }
