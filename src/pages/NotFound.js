5f346b44b01cc0bc4a6c611eaa6cc57bc8a30209
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-03-04 13:56:26 +0300

    fixed styling of 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 export default function NotFound () {
     return (
         <div className="not-found">
-            <h2>404</h2>
+            <div className="warning">
+                <h2>404</h2>
             <p>Page Not Found 💔 </p>
+            <hr/>
+            </div>
+            
+            <Link to ="/">
+                <p className="statement">
+                    Go back to home-page
+                </p>
+            </Link>
+
         </div>
     );
 }
