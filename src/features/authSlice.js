86b1599a8932d35e2cff444a4a851c0371ef8f72
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-03-08 14:24:46 +0300

    created an authentication reducer

---
+++ src/features/authSlice.js
@@ -0,0 +1,24 @@
+import {createSlice} from '@redux-toolkit';
+
+const initialState = {
+    token: localStorage.getItem("token"),
+    name: "",
+    email: "",
+    _id: "",
+    registerStatus: "",
+    registerError: "",
+    loginStatus: "",
+    loginError: "",
+    userLoaded: false,
+}
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {},
+    extraReducers: {},
+
+})
+
+export default authSlice.reducer;
