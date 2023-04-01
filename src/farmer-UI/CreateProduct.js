b7e847ce7bd5500b62664e60bc162475b9a547b0
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-01 16:59:21 +0300

    fixed am error

--- src/farmer-UI/CreateProduct.js
+++ src/farmer-UI/CreateProduct.js
@@ -1,22 +1,77 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { productsCreate } from '../features/productsSlice';
 import './Farmer.css';
 
 export default function CreateProduct (){
+    
+    const dispatch = useDispatch();
 
     const [productImg, setproductImg] = useState("");
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+
+    console.log(productImg);
 
     const handleProductImageUpload = (e) =>{
 
         const file = e.target.files[0];
+
+        TransformFile(file)
+    }
+
+    const TransformFile = (file) => {
+
+        const reader = new FileReader()
+
+        if(file){
+            reader.readAsDataURL(file)
+            reader.onloadend = () => {
+                setproductImg(reader.result)
+            }
+        }else {
+            setproductImg("")
+        }
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(productsCreate)({
+            name,
+            price,
+            category,
+            image : productImg
+        })
+
+
+
     }
 
     return (
         <div className="create-product">
-            <div className="styled-form">
+            <div className="styled-form" onSubmit={handleSubmit}>
                 <h3>Create a product</h3>
-                <input type="file" accept="image/" onChange={handleProductImageUpload} />
+                <input type="file" accept="image/" onChange={handleProductImageUpload} required/>
+                <select onChange={(e) => setCategory(e.target.value)} required>
+                    <option value="">Select Category</option>
+                    <option value="fruit">Fruit</option>
+                    <option value="vegetable">Vegetable</option>
+
+                </select>
+                <input type="text" required placeholder="Name" onChange ={(e) => setName(e.target.value)} />
+                <input type="text" required placeholder="Price in kshs" onChange={(e) => setPrice(e.target.value)}/>
+                <button className="create-submit" type="submit">Submit
+
+                </button>
             </div>
-            <div className="image-preview"></div>
+            <div className="image-preview">
+                { productImg ?
+                 <div>
+                 <img src= { productImg } alt="uploaded product"/></div> : 
+                 <p> Image preview will appear here</p>}
+            </div>
         </div>
     )
 }
--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -1,9 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import { url } from './api';
 
 const initialState = {
   items: [],
   status: null,
+  createStatus: null, 
   
 };
 
@@ -13,6 +16,23 @@ const initialState = {
     
     const response = await axios.fetchById('http://localhost:5000/products');
     return response.data;
+    
+  }
+);
+
+   export const productsCreate = createAsyncThunk(
+  'products/productsCreate',
+  async (values) => {
+    try {
+      const response = await axios.post(`${url}/products`, values);
+    return response.data;
+
+    }catch(error) {
+      console.log(error);
+      toast.error(error.response?.data)
+    }
+    
+    
     
   }
 );
@@ -34,6 +54,18 @@ const productsSlice = createSlice({
     },
     [productsFetch.rejected]: (state, action) => {
       state.status = 'rejected';
+      
+
+    },
+    [productsCreate.pending]: (state, action) => {
+      state.createStatus = 'pending';
+    },
+    [productsCreate.fulfilled]: (state, action) => {
+      state.createStatus = 'success';
+      state.items.push(action.payload);
+    },
+    [productsCreate.rejected]: (state, action) => {
+      state.createStatus = 'rejected';
       
 
     },
