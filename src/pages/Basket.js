fcc8a6d00a7d5af1904327c0810865983e3bf552
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-13 06:10:50 +0300

    fixed errors

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -6,70 +6,71 @@ import { url } from '../features/api';
 import './Basket.css';
 import { addToBasket } from '../features/basketSlice';
 
-export default function Basket () {
+export default function Basket() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { items: data, status } = useSelector((state) => state.products);
 
-    const { items: data, status } = useSelector((state) => state.products);
-    const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [fetchedData, setFetchedData] = useState([]);
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${url}/products`);
+      const result = await response.json();
+      setIsLoading(false);
+      setFetchedData(result);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [fetchedData, setFetchedData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        try {
-            setIsLoading(true);
-            const response = await fetch(`${url}/products`);
-            const result = await response.json();
-            setIsLoading(false);
-            setFetchedData(result);
-        } catch (error) {
-            setIsLoading(false);
-            setError(error.message);
-        }
-    };
+  const products = data || fetchedData;
 
-    useEffect(() => {
-        if (data) {
-            setProducts(data);
-        }
-    }, [data]);
+  const handleAddToBasket = (product) => {
+    dispatch(addToBasket(product));
+    navigate('/basket-one');
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const handleAddToBasket = (product) => {
-        dispatch(addToBasket(product));
-        navigate('/basket-one');
-    };
-
-
-    return (
-        <div className="basket">
-            <Link to="/basket-one" className="next-link">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"/>
-                </svg> Next
-            </Link>
-            { isLoading ? (<p>Loading....</p>) :
-                error ? (<p>an error occured</p>) :
-                    (<>
-                        <h2> Fresh Market</h2>
-                        <div className="products">
-                            {(data || fetchedData).map(product => <div key={product._id} className="product">
-                                <h3>{product.name}</h3>
-                                <img src={product.image.url} alt={product.name} />
-                                <div className="details">
-                                    <span>{product.desc}</span>
-                                    <span className="price">Kshs {product.price}</span>
-                                </div>
-                                <button onClick= {() => handleAddToBasket(product)}>Add to Basket</button>
-
-                            </div>)}
-                        </div>
-                        
-                    </>)
-                    }</div>)};
+  return (
+    <div className="basket">
+      <Link to="/basket-one" className="next-link">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z" />
+        </svg>{' '}
+        Next
+      </Link>
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : error ? (
+        <p>an error occurred</p>
+      ) : (
+        <>
+          <h2> Fresh Market</h2>
+          <div className="products">
+            {products.map((product) => (
+              <div key={product._id} className="product">
+                <h3>{product.name}</h3>
+                <img src={product.image.url} alt={product.name} />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span className="price">Kshs {product.price}</span>
+                </div>
+                <button onClick={() => handleAddToBasket(product)}>
+                  Add to Basket
+                </button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
