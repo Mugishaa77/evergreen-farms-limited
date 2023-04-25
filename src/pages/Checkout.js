f6c5b850cbdc967c72fc715819652e467cc47a1b
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-25 17:11:13 +0300

    styled the checkout component

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -26,7 +26,7 @@ export default function Checkout () {
               }`}
             >
               <span className="index">{index + 1} </span>
-              <p>Address Details</p>
+              <p>View Basket</p>
             </div>
           )}
         </Step>
@@ -61,7 +61,10 @@ export default function Checkout () {
                 accomplished ? "accomplished" : null
               }`}
             >
-              <span className="index"> {index + 1} </span>
+              <span className="order-complete">
+               <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+</svg> </span>
               <p> Order Complete</p>
             </div>
           )}
@@ -71,7 +74,7 @@ export default function Checkout () {
 
       {currentStep === 0 && (
         <div>
-          <p>Step 1: Address Details</p>
+          <p>Step 1: View Basket</p>
           <button onClick={handleNextStep}>Next Step</button>
         </div>
       )}
