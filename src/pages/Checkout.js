6a7344bc66a24e0cb8ad8647fb15e9cd2201693a
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-25 15:40:47 +0300

    added a progress bar to the checkout component

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,4 +1,5 @@
-
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
 import './Checkout.css';
 
 
@@ -8,6 +9,49 @@ export default function Checkout () {
     return (
         <div className="checkout">
             <h3> Checkout</h3>
+            <ProgressBar percent={75}>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        
+        <p>{index + 1} Address Details</p>
+      </div>
+    )}
+  </Step>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      > <p> {index + 1} Payment method</p>
+      </div>
+       )}
+       
+  </Step>
+  
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        
+        <p> {index + 1} Order Summary</p>
+      </div>
+    )}
+  </Step>
+  <Step>
+    {({ accomplished, index }) => (
+      <div
+        className={`indexedStep ${accomplished ? "accomplished" : null}`}
+      >
+        
+        <p> {index + 1} Order Complete</p>
+      </div>
+    )}
+  </Step>
+</ProgressBar>
+            
 
           
             
