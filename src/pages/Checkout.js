53d4f42b85c8e2025ea4b685ac9dbca115ca8da0
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-04-25 16:48:05 +0300

    added state to the progress bar

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "react-step-progress-bar/styles.css";
 import { ProgressBar, Step } from "react-step-progress-bar";
 import './Checkout.css';
@@ -5,53 +6,95 @@ import './Checkout.css';
 
 export default function Checkout () {
 
+    const [currentStep, setCurrentStep] = useState(0);
+
+  const handleNextStep = () => {
+    setCurrentStep(currentStep + 1);
+  };
+
+
  
     return (
         <div className="checkout">
             <h3> Checkout</h3>
-            <ProgressBar percent={75}>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
+             <ProgressBar percent={((currentStep + 1) / 4) * 100} unfilledBackground="#D8F7CA" filledBackground="#93B961" >
+        <Step>
+          {({ accomplished, index }) => (
+            <div
+              className={`indexedStep ${
+                accomplished ? "accomplished" : null
+              }`}
+            >
+              <span className="index">{index + 1} </span>
+              <p>Address Details</p>
+            </div>
+          )}
+        </Step>
+        <Step>
+          {({ accomplished, index }) => (
+            <div
+              className={`indexedStep ${
+                accomplished ? "accomplished" : null
+              }`}
+            >
+              <span className="index"> {index + 1} </span>{" "}
+              <p> Payment method</p>
+            </div>
+          )}
+        </Step>
+        <Step>
+          {({ accomplished, index }) => (
+            <div
+              className={`indexedStep ${
+                accomplished ? "accomplished" : null
+              }`}
+            >
+              <span className="index"> {index + 1} </span>
+              <p>Order Summary</p>
+            </div>
+          )}
+        </Step>
+        <Step>
+          {({ accomplished, index }) => (
+            <div
+              className={`indexedStep ${
+                accomplished ? "accomplished" : null
+              }`}
+            >
+              <span className="index"> {index + 1} </span>
+              <p> Order Complete</p>
+            </div>
+          )}
+        </Step>
+      </ProgressBar>
+      <div className="last-check">
+
+      {currentStep === 0 && (
+        <div>
+          <p>Step 1: Address Details</p>
+          <button onClick={handleNextStep}>Next Step</button>
+        </div>
+      )}
+      {currentStep === 1 && (
+        <div>
+          <p>Step 2: Payment method</p>
+          <button onClick={handleNextStep}>Next Step</button>
+        </div>
+      )}
+      {currentStep === 2 && (
+        <div>
+          <p>Step 3: Order Summary</p>
+          <button onClick={handleNextStep}>Next Step</button>
+        </div>
+      )}
+      {currentStep === 3 && (
+        <div>
+          <p>Step 4: Order Complete</p>
+          <button onClick={() => setCurrentStep(0)}>Restart</button>
+          </div>
         
-        <p>{index + 1} Address Details</p>
+      )}
       </div>
-    )}
-  </Step>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      > <p> {index + 1} Payment method</p>
-      </div>
-       )}
-       
-  </Step>
-  
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
-        
-        <p> {index + 1} Order Summary</p>
-      </div>
-    )}
-  </Step>
-  <Step>
-    {({ accomplished, index }) => (
-      <div
-        className={`indexedStep ${accomplished ? "accomplished" : null}`}
-      >
-        
-        <p> {index + 1} Order Complete</p>
-      </div>
-    )}
-  </Step>
-</ProgressBar>
-            
 
           
             
