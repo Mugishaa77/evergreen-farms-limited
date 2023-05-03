74c690cd5c53f7741daca6c7c3e5f8fe76d04190
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-05-03 21:11:45 +0300

    ...

--- src/farmer-UI/FarmerProfile.js
+++ src/farmer-UI/FarmerProfile.js
@@ -3,12 +3,16 @@ import './Farmer.css';
 export default function FarmerProfile (farmer) {
   return (
     <div className="farmer-profile">
-
+       <h1>Stall Details</h1>
       <h2>{farmer.stallName}</h2>
-      <h1>Stall {farmer.stallNumber}</h1>
-      <h3>Contact information</h3>
+       <h2>Stall Number:{farmer.stallNumber}</h2>
+
+      <h3> Farmer's Contact information</h3>
       <p>Name: {farmer.fullName} </p>
-      <p>Email: {farmer.emailAddress}</p>
+      <p>Email:<a href="mailto:{farmer.emailAddress}"
+                title="Email"
+                rel="noopener noreferrer"
+                className="email-link"></a> {farmer.emailAddress}</p>
       <p>Phone: {farmer.contactNumber}</p>
 
     </div>
--- src/grocer-ui/GrocerProfile.js
+++ src/grocer-ui/GrocerProfile.js
@@ -3,12 +3,16 @@ import './Grocer.css';
 export default function GrocerProfile (grocer) {
   return (
     <div className="grocer-profile">
-
+         <h1>Stall Details</h1>
       <h2>{grocer.stallName}</h2>
-      <h1>Stall {grocer.stallNumber}</h1>
-      <h3>Contact information</h3>
+      <h2>Stall Number:  {grocer.stallNumber}</h2>
+
+      <h3>Grocer's Contact information</h3>
       <p>Name: {grocer.fullName} </p>
-      <p>Email: {grocer.emailAddress}</p>
+      <p>Email:<a href="mailto:{grocer.emailAddress}"
+                title="Email"
+                rel="noopener noreferrer"
+                className="email-link"> {grocer.emailAddress}</a></p>
       <p>Phone: {grocer.contactNumber}</p>
 
     </div>
