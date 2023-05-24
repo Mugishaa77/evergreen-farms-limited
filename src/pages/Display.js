6b2d5568d194f12ebcb38a80a95319f1e48091c8
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-05-24 22:54:34 +0300

    necessary enhancements

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -27,7 +27,7 @@ export default function Display (){
             </Link>
             <h3>Main Menu</h3>
 
-  <p><span>Find what you are looking for</span> 
+  <p className="find"><span>Find what you are looking for</span> 
   <svg xmlns="http://www.w3.org/2000/svg" 
   width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
