a71bf16cd76aed67cbdd5c5db1914acb9c239ca2
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-05-29 15:34:02 +0300

    styled the farmer-profile component

--- src/farmer-UI/FarmerProfile.js
+++ src/farmer-UI/FarmerProfile.js
@@ -3,11 +3,11 @@ import './Farmer.css';
 export default function FarmerProfile (farmer) {
   return (
     <div className="farmer-profile">
-       <h1>Stall Details</h1>
+       <h3>Stall Details</h3>
       <h2>Stall Name: {farmer.stallName}</h2>
        <h2>Stall Number: {farmer.stallNumber}</h2>
 
-      <h1 className="contact-info"> Farmer's Contact information</h1>
+      <h3 className="contact-info"> Farmer's Contact information</h3>
       <p>Name: {farmer.fullName} </p>
       <p>Email:<a href="mailto:{farmer.emailAddress}"
                 title="Email"
