be979008317286b241989f1c38300f23f73b234c
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-05-31 09:37:46 +0300

    Updates vendor summary componenets

--- src/farmer-UI/Summary.js
+++ src/farmer-UI/Summary.js
@@ -4,6 +4,16 @@ export default function Summary (){
     return(
         <div className="summary">
         <h3>Summary</h3>
-        <p>Sales summary set to appear here</p>
+        <p>The sales information of the farmer is contained here which entails:
+            <ul>
+                <li>Products sold</li>
+                <li>Products bought</li>
+                <li>Customer payment details</li>
+                <li>Orders received</li>
+                <li>Upto date realtime information on sales activity</li>
+                  <li>Built-in marketing and promotion capabilities</li>
+                  <li>Perfomance tracking(tracks whether farmer is making losses or profits)</li>
+            </ul>
+        </p>
     </div>)
 }
--- src/grocer-ui/GrocerSummary.js
+++ src/grocer-ui/GrocerSummary.js
@@ -1,6 +1,16 @@
 export default function GrocerSummary (){
     return(<div className="grocer-summary">
         <h3>Summary</h3>
-        <p>Sales summary set to appear here</p>
+         <p>The sales information of the grocer is contained here which entails:
+            <ul>
+                <li>Products sold</li>
+                <li>Products bought</li>
+                <li>Customer payment details</li>
+                <li>Orders received</li>
+                <li>Upto date realtime information on sales activity</li>
+                  <li>Built-in marketing and promotion capabilities</li>
+                  <li>Perfomance tracking(tracks whether grocer is making losses or profits)</li>
+            </ul>
+        </p>
     </div>)
 }
